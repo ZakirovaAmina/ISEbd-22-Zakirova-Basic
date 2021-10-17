87e70edec597b121d13807e9cdcd2448438520ec
Амина Закирова <zakirova200272@gmail.com>
2021-10-17 13:58:43 +0400

    lab1

---
+++ Zakirova/DumpTruck.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Zakirova
+{
+    /// <summary>
+    /// Класс отрисовки самовсвала
+    /// </summary>
+    class DumpTruck
+	{
+        /// <summary>
+        /// Левая координата отрисовки самосвала
+        /// </summary>
+        private float _startPosX;
+        /// <summary>
+        /// Правая кооридната отрисовки самосвала
+        /// </summary>
+        private float _startPosY;
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private int _pictureWidth = 100;
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private int _pictureHeight = 100;
+
+        /// <summary>
+        /// Ширина отрисовки самосвала
+        /// </summary>
+        private readonly int truckWidth = 100;
+        /// <summary>
+        /// Высота отрисовки самосвала
+        /// </summary>
+        private readonly int truckHeight = 60;
+        /// <summary>
+        /// Максимальная скорость
+        /// </summary>
+        public int MaxSpeed { private set; get; }
+        /// <summary>
+        /// Вес самосвала
+        /// </summary>
+        public float Weight { private set; get; }
+        /// <summary>
+        /// Основной цвет кузова
+        /// </summary>
+        public Color MainColor { private set; get; }
+        /// <summary>
+        /// Дополнительный цвет
+        /// </summary>
+        public Color DopColor { private set; get; }
+        /// <summary>
+        /// Признак наличия трубы
+        /// </summary>
+        public bool Duct { private set; get; }
+        /// <summary>
+        /// Признак наличия кузова
+        /// </summary>
+        public bool Carcase { private set; get; }
+        /// <summary>
+        /// Признак наличия передней фары
+        /// </summary>
+        public bool FrontLight { private set; get; }
+        /// <summary>
+        /// Признак наличия задней фары
+        /// </summary>
+        public bool BackLight { private set; get; }
+        /// <summary>
+        /// Инициализация свойств
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес самосвала</param>
+        /// <param name="mainColor">Основной цвет кузова</param>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="duct">Признак наличия трубы</param>
+        /// <param name="carcase">Признак наличия кузова</param>
+        /// <param name="frontLight">Признак наличия передней фары</param>
+        /// <param name="backLight">Признак наличия задней фары</param>
+        public void Init(int maxSpeed, float weight, Color mainColor, Color dopColor,
+       bool duct, bool carcase, bool frontLight, bool backLight)
+        {
+            MaxSpeed = maxSpeed;
+            
+        Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            Duct = duct;
+            Carcase = carcase;
+            FrontLight = frontLight;
+            BackLight = backLight;
+        }
+        /// <summary>
+        /// Установка позиции автомобиля
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        /// <param name="width">Ширина картинки</param>
+        /// <param name="height">Высота картинки</param>
+        public void SetPosition(int x, int y, int width, int height)
+        {           
+            _startPosX = x;
+            _startPosY = y;
+            _pictureHeight = height;
+            _pictureWidth = width;
+        }
+        /// <summary>
+        /// Изменение направления пермещения
+        /// </summary>
+        /// <param name="direction">Направление</param>
+        public void MoveTransport(Direction direction)
+        {
+            int scale = 63;
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - truckWidth - scale) { _startPosX += step;}                    
+                    break;
+
+                //влево
+                case Direction.Left:                 
+                    if (_startPosX - step > scale) { _startPosX -= step; }                    
+                    break;
+
+                //вверх
+                case Direction.Up:                    
+                    if(_startPosY - step > scale) { _startPosY -= step; }                   
+                    break;
+
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - truckHeight) { _startPosY += step; }                    
+                    break;
+            }
+        }
+        /// <summary>
+        /// Отрисовка самосвала
+        /// </summary>
+        /// <param name="g"></param>
+        public void DrawTransport(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            Pen wheel = new Pen(Color.LightBlue);
+            Pen fog1 = new Pen(Color.Gray);
+            Pen back1 = new Pen(Color.LightGray);
+            Pen light1 = new Pen(Color.Yellow);
+            Brush fond = new SolidBrush(Color.Black);
+            Brush light = new SolidBrush(Color.Yellow);
+            Brush wheels = new SolidBrush(Color.LightBlue);
+            Brush fog = new SolidBrush(Color.Gray);
+            Brush back = new SolidBrush(Color.LightGray);
+
+            g.DrawRectangle(pen, _startPosX, _startPosY, 100, 25);
+            g.DrawRectangle(pen, _startPosX + 60, _startPosY - 40, 40, 37);
+            g.DrawRectangle(pen, _startPosX + 65, _startPosY - 35, 20, 16);
+
+            g.FillRectangle(fond, _startPosX, _startPosY, 100, 25);
+            g.FillRectangle(fond, _startPosX + 60, _startPosY - 40, 40, 37);
+
+            g.DrawEllipse(wheel, _startPosX + 75, _startPosY + 24, 20, 20);
+            g.DrawEllipse(wheel, _startPosX + 20, _startPosY + 24, 20, 20);
+            g.DrawEllipse(wheel, _startPosX, _startPosY + 24, 20, 20);
+
+            g.FillEllipse(wheels, _startPosX + 75, _startPosY + 24, 20, 20);
+            g.FillEllipse(wheels, _startPosX + 20, _startPosY + 24, 20, 20);
+            g.FillEllipse(wheels, _startPosX, _startPosY + 24, 20, 20);
+
+            if (FrontLight)
+            {
+                g.DrawEllipse(light1, _startPosX + 100, _startPosY, 65, 20);
+                g.FillEllipse(light, _startPosX + 100, _startPosY, 65, 20);
+            }
+            if (BackLight)
+            {
+                g.DrawEllipse(light1, _startPosX - 65, _startPosY + 5, 65, 20);
+                g.FillEllipse(light, _startPosX - 65, _startPosY + 5, 65, 20);
+            }
+            if (Duct)
+            {
+                g.DrawRectangle(fog1, _startPosX + 90, _startPosY - 57, 10, 16);
+                g.FillRectangle(fog, _startPosX + 90, _startPosY - 57, 10, 16);
+            }
+            if (Carcase)
+            {
+                g.DrawRectangle(back1, _startPosX, _startPosY - 30, 60, 40);
+                g.FillRectangle(back, _startPosX, _startPosY - 30, 60, 40);
+            }
+            
+        }
+
+    }
+
+}
---
+++ Zakirova/FormTruck.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Zakirova
+{
+    partial class FormTruck
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.pictureBoxTruck = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTruck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::Zakirova.Properties.Resources.arrowU;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(734, 384);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::Zakirova.Properties.Resources.arrowL;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(698, 420);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::Zakirova.Properties.Resources.arrow;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(770, 420);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 5;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::Zakirova.Properties.Resources.arrowd;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonDown.Location = new System.Drawing.Point(734, 420);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxTruck
+            // 
+            this.pictureBoxTruck.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxTruck.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxTruck.Name = "pictureBoxTruck";
+            this.pictureBoxTruck.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxTruck.TabIndex = 6;
+            this.pictureBoxTruck.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxTruck);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dump truck";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTruck)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.PictureBox pictureBoxTruck;
+    }
+}
+
